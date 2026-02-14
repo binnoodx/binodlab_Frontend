a744fc337fc5c83e1d9869ade36b0f5ab52a7a70
Binod Sharma <thebinod404@gmail.com>
2026-02-15 00:19:28 +0545

    Managing Commits

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,7 +1,6 @@
 // socket.js (or socket.ts)
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.BACKEND_URI, {
+export const socket = io("http://localhost:3001", {
   transports: ["websocket"],
-  autoConnect:true
 });
